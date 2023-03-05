2e6b2d1f59e0cdea9b1179219ebc4f5ce189b380
melt <thomas.meltzer1@gmail.com>
2023-03-05 11:09:26 +0000

    integrate ctests into CI workflow

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,8 @@
+set -e
+rm -rf ./build-*
+
 cmake -B ./build-gs -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D USEGS=ON
+(cd ./build-gs && make -j 4 && ctest -V)
 
 cmake -B ./build-lp -D CMAKE_C_COMPILER=gcc -D CMAKE_CXX_COMPILER=g++ -D USEGS=OFF
+(cd ./build-lp && make -j 4 && ctest -V)
